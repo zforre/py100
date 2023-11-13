a1aaa10d398be9cd457668bb7a92e8ff57f08580
zforre <zachforre@gmail.com>
2023-11-13 18:41:49 -0500

    changes alignment of table

--- class_object_practice/main.py
+++ class_object_practice/main.py
@@ -16,5 +16,6 @@ table = PrettyTable()
 
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
 
 print(table)
